b69e62372c7e1c6fe7635349930b3f94b79d746b
Jakub Rosa <arnoldpepek@gmail.com>
2026-07-13 17:18:11 +0200

    Add port to mysqli connection

--- pages/mysqli.php
+++ pages/mysqli.php
@@ -3,5 +3,6 @@
   $username = 'user_c7c14afd';
   $password = 'pw_gvLjHCnL0W6FgH81DX8jhSg5PUbqCVg2';
   $database = 'db_1c86953c';
-  $connect = mysqli_connect($host, $username, $password, $database);
+  $port = 10272;
+  $connect = mysqli_connect($host, $username, $password, $database, $port);
 ?>
